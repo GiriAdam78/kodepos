61a71cd7f46f53b91dd99a82c7f45488554cfb7b
Giridiwa319 <giriadam67@gmail.com>
2025-01-06 14:02:10 +0700

    Perbaikan Server

--- server.js
+++ server.js
@@ -21,14 +21,13 @@ app.use(
         origin: "*",
     })
 );
-app.get("/data-all", async (req,res) => {
- try{
-    const result = await pool.query('select * from kode_pos');
-    res.status(200).json(result.rows);
- }catch(error){
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
- }
+app.get('/kodepos', async (req, res) => {
+    try {
+        const results = await prisma.kodepos.findMany();
+        res.status(200).json(results);
+    } catch (error) {
+        res.status(500).json({ error: "Terjadi kesalahan saat mengambil data Kode Pos dan Kelurahan" });
+    }
 });
 
 // Route Post
